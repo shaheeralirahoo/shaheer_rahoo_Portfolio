64e7a950f3c1963f2b32d9820a114186a6eb3f4f
mike-byfield <168654442+mike-byfield@users.noreply.github.com>
2026-09-03 02:12:34 -0700

    delete  the  old

--- src/utils/axios.js
+++
@@ -1,8 +0,0 @@
-// src/utils/axios.js
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://api-carseller.vercel.app/api", // or your deployed API URL
-});
-
-export default api;
